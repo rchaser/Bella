1308fe6c4542088acc518787fb20c48a66e2710f
MrBear <mrbear141@live.com>
2016-07-04 03:14:45 +0300

    ConsoleBella is started

---
+++ Bella/src/BellaConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BellaConsole.UserInterface;
+
+namespace BellaConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
